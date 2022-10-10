83b3a0f9e354dba5899407bbb87c92472a7f1917
Sean <sr3463@drexel.edu>
2022-10-09 23:11:46 -0400

    Added Dockerfile; changed server.py to run on port 5000 for testing

--- server/server.py
+++ server/server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,4 +8,6 @@ def members():
 	return {"test": ["test1", "test2", "test3"]}
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(debug=True, host='0.0.0.0', port=port)
+	# app.run(debug=True)
