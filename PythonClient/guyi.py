98ca6d237b1dc753ffa161875873726bc6550d5f
samuelt525 <samuelt525@gmail.com>
2022-11-15 19:01:55 -0500

    Added Video Player GUI

--- PythonClient/guyi.py
+++ PythonClient/guyi.py
@@ -1,6 +1,9 @@
 import sys
 import os
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QSize, Qt, QUrl
+from PyQt6.QtMultimedia import QAudioOutput, QMediaPlayer
+from PyQt6.QtQml import QQmlApplicationEngine
+from PyQt6.QtQuickWidgets import QQuickWidget
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog,QLineEdit,QFormLayout,QWidget
 import subprocess
 
@@ -10,22 +13,29 @@ class MainWindow(QWidget):
         self.filename = ''
         self.setWindowTitle("My App")
         self.filebutton = QPushButton("Select File")
+        self.filebutton.hasFocus()
         self.filebutton.clicked.connect(self.getfile)
         self.submissionbutton = QPushButton('Submit')
         self.submissionbutton.clicked.connect(self.processVideo)
+        self.player = 0
         
         self.rescaleRatio = QLineEdit()
         self.outputfps = QLineEdit()
         self.path = QLineEdit()
 
-        flo = QFormLayout()
-        flo.addRow('rescale ratio', self.rescaleRatio)
-        flo.addRow('output fps ', self.outputfps)
-        flo.addRow(self.filebutton)
-        flo.addRow(self.path)
-        flo.addRow(self.submissionbutton)
+        self.view = QQuickWidget()
+        self.view.setSource(QUrl.fromLocalFile('media.qml'))
 
-        self.setLayout(flo) 
+        self.flo = QFormLayout()
+        self.flo.addRow('rescale ratio', self.rescaleRatio)
+        self.flo.addRow('output fps ', self.outputfps)
+        self.flo.addRow(self.filebutton)
+        self.flo.addRow(self.path)
+        self.flo.addRow(self.submissionbutton)
+        self.flo.addRow(self.view)
+
+
+        self.setLayout(self.flo) 
 
     def getfile(self):
         self.filename = QFileDialog.getOpenFileUrl(self, 'Open file')
@@ -35,6 +45,11 @@ class MainWindow(QWidget):
             if self.outputfps.text() == '':
                 self.outputfps.setText('50')
             self.path.setText(self.filename[0].fileName())
+            player = self.view.rootObject().findChild(QMediaPlayer, "player")
+            player.setProperty('source', self.filename[0].path())
+            player.play()
+
+
     def processVideo(self):
         subprocess.Popen(['python3', '../video-compression/compression-test1.py', self.filename[0].path(), self.rescaleRatio.text(), self.outputfps.text()], env=os.environ)
 app = QApplication(sys.argv)
