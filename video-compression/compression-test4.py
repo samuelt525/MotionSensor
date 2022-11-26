53b94447111552359a7d95446483f61534091e0f
Humaid <28841450+humaid99@users.noreply.github.com>
2022-11-26 13:53:25 -0500

    updates to compression4

--- video-compression/compression-test4.py
+++ video-compression/compression-test4.py
@@ -1,26 +1,55 @@
-from scenedetect import SceneManager, open_video, ContentDetector
+from scenedetect import SceneManager, open_video, ContentDetector, split_video_ffmpeg
 import sys
 import csv
 
-def find_scenes(video_path, threshold=60):
-    video = open_video(video_path)
-    scene_manager = SceneManager()
+def find_scenes(videoPath, threshold=10, frameSkip=0, showProgress=True):
+    video = open_video(videoPath)
 
-    scene_manager.add_detector(ContentDetector(threshold=threshold))
+    # sceneManager
+    sceneManager = SceneManager()
+    
+    # Detector Settings
+    sceneManager.add_detector(ContentDetector(threshold=threshold, min_scene_len=1200))
 
     # Detect all scenes in video from current position to end.
-    scene_manager.detect_scenes(video, frame_skip=1, show_progress=True)
+    sceneManager.detect_scenes(video, frame_skip=frameSkip, show_progress=showProgress)
     
-    # `get_scene_list` returns a list of start/end timecode pairs for each scene that was found.
-    return scene_manager.get_scene_list()
-
-if __name__ == '__main__':
-    sceneList = find_scenes(sys.argv[1], int(sys.argv[2]))
-    # print(sceneList[0])
+    sceneList = sceneManager.get_scene_list()
 
     for i, scene in enumerate(sceneList):
-        print('Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (i+1,scene[0].get_timecode(), scene[0].get_frames(),scene[1].get_timecode(), scene[1].get_frames(),))
+        print('Scene %2d: Start %s / Frame %d, End %s / Frame %d' %
+              (i+1, scene[0].get_timecode(), scene[0].get_frames(), scene[1].get_timecode(), scene[1].get_frames(),))
 
-    with open('sample.txt', 'w') as file:
+    # write timecodes to file - maybe change to html output
+    with open('timecode.txt', 'w') as file:
         for i, scene in enumerate(sceneList):
-            file.write('Scene %2d: Start %s / Frame %d, End %s / Frame %d\n' % (i+1, scene[0].get_timecode(), scene[0].get_frames(), scene[1].get_timecode(), scene[1].get_frames(),))
+            file.write('Scene %2d: Start %s / Frame %d, End %s / Frame %d\n' %
+                       (i+1, scene[0].get_timecode(), scene[0].get_frames(), scene[1].get_timecode(), scene[1].get_frames(),))
+
+    # use ffmpeg for splitting video files
+    # TODO add func to output to directory and prevent overwrites on multiple runs
+    split_video_ffmpeg(videoPath, sceneList, show_progress=True)
+
+    return sceneList
+
+if __name__ == '__main__':
+
+    sceneListMain = find_scenes(sys.argv[1], int(sys.argv[2]))
+
+    # print(sys.argv)
+
+    # TODO deal with more args
+    # if len(sys.argv) < 2:
+    #     raise AttributeError('Video File Path not provided.')
+    # elif len(sys.argv) == 2:
+    #     sceneList = find_scenes(sys.argv[1])
+    # elif len(sys.argv) == 3:
+    #     sceneList = find_scenes(sys.argv[1], int(sys.argv[2]))
+    # elif len(sys.argv) == 4:
+    #     # sceneList = find_scenes(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    #     if sys.argv[3].isnumeric():
+    #         sceneList = find_scenes(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+    #     else:
+    #         sceneList = find_scenes(sys.argv[1], int(sys.argv[2]), show_progress=bool(sys.argv[3]))
+    # elif len(sys.argv) == 5:
+    #     sceneList = find_scenes(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]), False)
